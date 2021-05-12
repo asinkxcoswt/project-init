dbd500109cd142ccfb10df23621493fa62004ecd
asinkxcoswt <pipat@bluebik.com>
2021-05-13 02:30:52 +0700

    fix serverless-typescript-init.sh

--- serverless-typescript-init.sh
+++ serverless-typescript-init.sh
@@ -2,14 +2,49 @@
 
 set -e
 
+alert() {
+  _doc 'To print a normal message with a cute cow.' && return 1
+  if [ -n "${X_SILENT}" ]; then
+    return 0
+  fi  
+
+  echo ""
+  echo " ______________________
+ < $* >
+ ----------------------
+        \   ^__^
+         \  (..)\_______
+            (__)\       )\/
+                ||----w |
+                ||     ||"
+  echo ""
+}
+
+error() {
+  _doc 'To print an error message with a cute cow, and then exit the script with non-zero exit code'  && return 1
+  echo ""
+  echo " ______________________
+  < $* >
+ ----------------------
+        \   ^__^
+         \  (xx)\_______
+            (__)\       )\/
+             U  ||----w |
+                ||     ||" 1>&2
+
+  echo ""
+  exit 1
+}
+
+
+alert "Set up serverless typescript ..."
+
 yarn init -y
 yarn add serverless
-
 rm package.json
-
 npx sls create --template aws-nodejs-typescript
 
-echo "node_modules" > .gitignore
+alert "Set up eslint and prettier ..."
 
 yarn add --dev eslint-config-airbnb-base
 npx install-peerdeps --dev eslint-config-airbnb-base --yarn
@@ -46,10 +81,7 @@ jq '.settings."import/resolver".typescript.paths = "./tsconfig.json"' .eslintrc 
 echo "{}" >tsconfig.eslint.json
 jq '.extends = "./tsconfig.json"' tsconfig.eslint.json >tsconfig.eslint.json.tmp && mv tsconfig.eslint.json.tmp tsconfig.eslint.json
 
-echo "webpack.config.js" >>.eslintignore
-echo "commitlint.config.js" >>.eslintignore
-echo ".lintstagedrc" >>.prettierignore
-echo "yarn.lock" >>.prettierignore
+alert "Set up lint-staged ..."
 
 yarn add --dev lint-staged
 echo "{}" >.lintstagedrc
@@ -58,13 +90,27 @@ jq '."*"[1] = "npx prettier -c"' .lintstagedrc >.lintstagedrc.tmp && mv .lintsta
 jq '."*.{js,jsx,ts,tsx}"[0] = "npx eslint --fix"' .lintstagedrc >.lintstagedrc.tmp && mv .lintstagedrc.tmp .lintstagedrc
 jq '."*.{js,jsx,ts,tsx}"[1] = "npx eslint"' .lintstagedrc >.lintstagedrc.tmp && mv .lintstagedrc.tmp .lintstagedrc
 
+alert "Set up husky ..."
+
 yarn add --dev husky
 npx husky install
 npx husky add .husky/pre-commit "npx lint-staged"
 
+alert "Set up commitlint ..."
+
 yarn add --dev @commitlint/{config-conventional,cli}
 echo "module.exports = {extends: ['@commitlint/config-conventional']}" >commitlint.config.js
 npx husky add .husky/commit-msg "npx --no-install commitlint --edit $1"
+
+alert "Set up ignore stuff ..."
+
+echo "webpack.config.js" >>.eslintignore
+echo "commitlint.config.js" >>.eslintignore
+echo ".lintstagedrc" >>.prettierignore
+echo "yarn.lock" >>.prettierignore
+echo "node_modules" > .gitignore
+
+alert "Try to commit, please fix any error manually"
 
 git add --all
 git commit -m "chore: initialize project with asinkxcoswt/project-init/main/serverless-typescript-init.sh"
