6e2f3da14a96fea1a0ffe9f5a1721b80fa281afb
asinkxcoswt <pipat@bluebik.com>
2021-05-13 02:41:31 +0700

    fix serverless-typescript-init.sh

--- serverless-typescript-init.sh
+++ serverless-typescript-init.sh
@@ -1,116 +1,125 @@
 #!/bin/bash
 
-set -e
-
 alert() {
-  _doc 'To print a normal message with a cute cow.' && return 1
-  if [ -n "${X_SILENT}" ]; then
-    return 0
-  fi  
-
-  echo ""
-  echo " ______________________
- < $* >
- ----------------------
+    echo ""
+    echo " ______________________
+    < $* >
+    ----------------------
         \   ^__^
          \  (..)\_______
             (__)\       )\/
                 ||----w |
                 ||     ||"
-  echo ""
+    echo ""
 }
 
-error() {
-  _doc 'To print an error message with a cute cow, and then exit the script with non-zero exit code'  && return 1
-  echo ""
-  echo " ______________________
-  < $* >
- ----------------------
-        \   ^__^
-         \  (xx)\_______
-            (__)\       )\/
-             U  ||----w |
-                ||     ||" 1>&2
+setup_serverless() {
+    set -e
+    alert "Set up serverless typescript ..."
 
-  echo ""
-  exit 1
+    yarn init -y
+    yarn add serverless
+    rm package.json
+    npx sls create --template aws-nodejs-typescript
 }
 
+setup_eslint_prettier() {
+    set -e
+    alert "Set up eslint and prettier ..."
 
-alert "Set up serverless typescript ..."
+    yarn add --dev eslint-config-airbnb-base
+    npx install-peerdeps --dev eslint-config-airbnb-base --yarn
+    yarn add --dev \
+        eslint-config-prettier \
+        eslint-plugin-prettier \
+        @typescript-eslint/parser \
+        @typescript-eslint/eslint-plugin \
+        eslint-import-resolver-typescript \
+        prettier-plugin-sh
 
-yarn init -y
-yarn add serverless
-rm package.json
-npx sls create --template aws-nodejs-typescript
+    echo "{}" >.prettierrc
+    jq '.semi = true' .prettierrc >.prettierrc.tmp && mv .prettierrc.tmp .prettierrc
+    jq '.trailingComma = "all"' .prettierrc >.prettierrc.tmp && mv .prettierrc.tmp .prettierrc
+    jq '.singleQuote = true' .prettierrc >.prettierrc.tmp && mv .prettierrc.tmp .prettierrc
+    jq '.printWidth = 120' .prettierrc >.prettierrc.tmp && mv .prettierrc.tmp .prettierrc
+    jq '.tabWidth = 4' .prettierrc >.prettierrc.tmp && mv .prettierrc.tmp .prettierrc
+    jq '.plugins[0] = "prettier-plugin-sh"' .prettierrc >.prettierrc.tmp && mv .prettierrc.tmp .prettierrc
 
-alert "Set up eslint and prettier ..."
+    echo "{}" >.eslintrc
+    jq '.parser = "@typescript-eslint/parser"' .eslintrc >.eslintrc.tmp && mv .eslintrc.tmp .eslintrc
+    jq '.parserOptions.project = "./tsconfig.eslint.json"' .eslintrc >.eslintrc.tmp && mv .eslintrc.tmp .eslintrc
+    jq '.extends[0] = "airbnb-base"' .eslintrc >.eslintrc.tmp && mv .eslintrc.tmp .eslintrc
+    jq '.extends[1] = "plugin:@typescript-eslint/recommended"' .eslintrc >.eslintrc.tmp && mv .eslintrc.tmp .eslintrc
+    jq '.extends[2] = "prettier"' .eslintrc >.eslintrc.tmp && mv .eslintrc.tmp .eslintrc
+    jq '.plugins[0] = "@typescript-eslint"' .eslintrc >.eslintrc.tmp && mv .eslintrc.tmp .eslintrc
+    jq '.plugins[1] = "prettier"' .eslintrc >.eslintrc.tmp && mv .eslintrc.tmp .eslintrc
+    jq '.rules."@typescript-eslint/no-floating-promises" = "error"' .eslintrc >.eslintrc.tmp && mv .eslintrc.tmp .eslintrc
+    jq '.rules."import/extensions" = "off"' .eslintrc >.eslintrc.tmp && mv .eslintrc.tmp .eslintrc
+    jq '.rules."import/prefer-default-export" = "off"' .eslintrc >.eslintrc.tmp && mv .eslintrc.tmp .eslintrc
+    jq '.settings."import/resolver".typescript.alwaysTryTypes = true' .eslintrc >.eslintrc.tmp && mv .eslintrc.tmp .eslintrc
+    jq '.settings."import/resolver".typescript.paths = "./tsconfig.json"' .eslintrc >.eslintrc.tmp && mv .eslintrc.tmp .eslintrc
 
-yarn add --dev eslint-config-airbnb-base
-npx install-peerdeps --dev eslint-config-airbnb-base --yarn
-yarn add --dev \
-    eslint-config-prettier \
-    eslint-plugin-prettier \
-    @typescript-eslint/parser \
-    @typescript-eslint/eslint-plugin \
-    eslint-import-resolver-typescript \
-    prettier-plugin-sh
+    echo "{}" >tsconfig.eslint.json
+    jq '.extends = "./tsconfig.json"' tsconfig.eslint.json >tsconfig.eslint.json.tmp && mv tsconfig.eslint.json.tmp tsconfig.eslint.json
 
-echo "{}" >.prettierrc
-jq '.semi = true' .prettierrc >.prettierrc.tmp && mv .prettierrc.tmp .prettierrc
-jq '.trailingComma = "all"' .prettierrc >.prettierrc.tmp && mv .prettierrc.tmp .prettierrc
-jq '.singleQuote = true' .prettierrc >.prettierrc.tmp && mv .prettierrc.tmp .prettierrc
-jq '.printWidth = 120' .prettierrc >.prettierrc.tmp && mv .prettierrc.tmp .prettierrc
-jq '.tabWidth = 4' .prettierrc >.prettierrc.tmp && mv .prettierrc.tmp .prettierrc
-jq '.plugins[0] = "prettier-plugin-sh"' .prettierrc >.prettierrc.tmp && mv .prettierrc.tmp .prettierrc
+}
 
-echo "{}" >.eslintrc
-jq '.parser = "@typescript-eslint/parser"' .eslintrc >.eslintrc.tmp && mv .eslintrc.tmp .eslintrc
-jq '.parserOptions.project = "./tsconfig.eslint.json"' .eslintrc >.eslintrc.tmp && mv .eslintrc.tmp .eslintrc
-jq '.extends[0] = "airbnb-base"' .eslintrc >.eslintrc.tmp && mv .eslintrc.tmp .eslintrc
-jq '.extends[1] = "plugin:@typescript-eslint/recommended"' .eslintrc >.eslintrc.tmp && mv .eslintrc.tmp .eslintrc
-jq '.extends[2] = "prettier"' .eslintrc >.eslintrc.tmp && mv .eslintrc.tmp .eslintrc
-jq '.plugins[0] = "@typescript-eslint"' .eslintrc >.eslintrc.tmp && mv .eslintrc.tmp .eslintrc
-jq '.plugins[1] = "prettier"' .eslintrc >.eslintrc.tmp && mv .eslintrc.tmp .eslintrc
-jq '.rules."@typescript-eslint/no-floating-promises" = "error"' .eslintrc >.eslintrc.tmp && mv .eslintrc.tmp .eslintrc
-jq '.rules."import/extensions" = "off"' .eslintrc >.eslintrc.tmp && mv .eslintrc.tmp .eslintrc
-jq '.rules."import/prefer-default-export" = "off"' .eslintrc >.eslintrc.tmp && mv .eslintrc.tmp .eslintrc
-jq '.settings."import/resolver".typescript.alwaysTryTypes = true' .eslintrc >.eslintrc.tmp && mv .eslintrc.tmp .eslintrc
-jq '.settings."import/resolver".typescript.paths = "./tsconfig.json"' .eslintrc >.eslintrc.tmp && mv .eslintrc.tmp .eslintrc
+setup_lint_staged() {
+    set -e
+    alert "Set up lint-staged ..."
 
-echo "{}" >tsconfig.eslint.json
-jq '.extends = "./tsconfig.json"' tsconfig.eslint.json >tsconfig.eslint.json.tmp && mv tsconfig.eslint.json.tmp tsconfig.eslint.json
+    yarn add --dev lint-staged
+    echo "{}" >.lintstagedrc
+    jq '."*"[0] = "npx prettier --write"' .lintstagedrc >.lintstagedrc.tmp && mv .lintstagedrc.tmp .lintstagedrc
+    jq '."*"[1] = "npx prettier -c"' .lintstagedrc >.lintstagedrc.tmp && mv .lintstagedrc.tmp .lintstagedrc
+    jq '."*.{js,jsx,ts,tsx}"[0] = "npx eslint --fix"' .lintstagedrc >.lintstagedrc.tmp && mv .lintstagedrc.tmp .lintstagedrc
+    jq '."*.{js,jsx,ts,tsx}"[1] = "npx eslint"' .lintstagedrc >.lintstagedrc.tmp && mv .lintstagedrc.tmp .lintstagedrc
 
-alert "Set up lint-staged ..."
+}
 
-yarn add --dev lint-staged
-echo "{}" >.lintstagedrc
-jq '."*"[0] = "npx prettier --write"' .lintstagedrc >.lintstagedrc.tmp && mv .lintstagedrc.tmp .lintstagedrc
-jq '."*"[1] = "npx prettier -c"' .lintstagedrc >.lintstagedrc.tmp && mv .lintstagedrc.tmp .lintstagedrc
-jq '."*.{js,jsx,ts,tsx}"[0] = "npx eslint --fix"' .lintstagedrc >.lintstagedrc.tmp && mv .lintstagedrc.tmp .lintstagedrc
-jq '."*.{js,jsx,ts,tsx}"[1] = "npx eslint"' .lintstagedrc >.lintstagedrc.tmp && mv .lintstagedrc.tmp .lintstagedrc
+setup_husky() {
+    set -e
+    alert "Set up husky ..."
 
-alert "Set up husky ..."
+    yarn add --dev husky
+    npx husky install
+    npx husky add .husky/pre-commit "npx lint-staged"
+}
 
-yarn add --dev husky
-npx husky install
-npx husky add .husky/pre-commit "npx lint-staged"
+setup_commitlint() {
+    set -e
+    alert "Set up commitlint ..."
 
-alert "Set up commitlint ..."
+    yarn add --dev @commitlint/{config-conventional,cli}
+    echo "module.exports = {extends: ['@commitlint/config-conventional']}" >commitlint.config.js
+    npx husky add .husky/commit-msg "npx --no-install commitlint --edit $1"
 
-yarn add --dev @commitlint/{config-conventional,cli}
-echo "module.exports = {extends: ['@commitlint/config-conventional']}" >commitlint.config.js
-npx husky add .husky/commit-msg "npx --no-install commitlint --edit $1"
+}
 
-alert "Set up ignore stuff ..."
+setup_ignore() {
+    set -e
+    alert "Set up ignore stuff ..."
 
-echo "webpack.config.js" >>.eslintignore
-echo "commitlint.config.js" >>.eslintignore
-echo ".lintstagedrc" >>.prettierignore
-echo "yarn.lock" >>.prettierignore
-echo "node_modules" > .gitignore
+    echo "webpack.config.js" >>.eslintignore
+    echo "commitlint.config.js" >>.eslintignore
+    echo ".lintstagedrc" >>.prettierignore
+    echo "yarn.lock" >>.prettierignore
+    echo "node_modules" > .gitignore
+}
 
-alert "Try to commit, please fix any error manually"
+setup_git() {
+    set -e
+    alert "Try to commit, please fix any error manually"
 
-git add --all
-git commit -m "chore: initialize project with asinkxcoswt/project-init/main/serverless-typescript-init.sh"
+    git init
+    git add --all
+    git commit -m "chore: initialize project with asinkxcoswt/project-init/main/serverless-typescript-init.sh"
+}
+
+setup_serverless \
+&& setup_eslint_prettier \
+&& setup_lint_staged \
+&& setup_husky \
+&& setup_commitlint \
+&& setup_ignore \
+&& setup_git
